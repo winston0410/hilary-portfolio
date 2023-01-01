88d9252b2008b03402a8f67cabac0f1864121863
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2023-01-01 22:27:05 +0000

    update

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<div class="outer-container">
+    <main>
+        <slot />
+    </main>
+    <aside>
+        <p>this is side bar</p>
+    </aside>
+</div>
+
+<style>
+    .outer-container {
+        padding: 1rem;
+        display: grid;
+        grid-template-columns: 0.7fr 0.3fr;
+        column-gap: 5rem;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,59 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts" context="module">
+
+</script>
+
+<script lang="ts">
+    const education = [{
+        name: "foo bar",
+        from: "Sept 2008",
+        to: "Jan 2010"
+    }]
+
+    const experience = [{
+        name: "Freelance Translator",
+        descriptions: [
+            'Historical, journal, literature translation (Mandarin-English, Cantonese-English)',
+            'Transcription (Mandarin-English,Cantonese-English)',
+            'Subtitling (Mandarin-English,Cantonese-English)',
+            'Company: Elite Asia, TED, SOAS University of London'
+        ],
+        from: "Sep 2022",
+        to: "Present"
+    }]
+</script>
+
+<section>
+    <h2>Personal profile</h2>
+    <p>A highly positive, enthusiastic and adaptive Chinese-English translator who has just finished her master!s degree. A person who loves challenges and learning new things, being able to work under pressure. A good communicator with people as well as building good relationships. A person who loves sharing knowledge with others and has previous experience in tutoring, and cooperating with colleagues and members.</p>
+</section>
+
+<section>
+    <h2>Education</h2>
+    <ul>
+        {#each education as { name, from, to}}
+        <li>
+            <p>{name}</p>
+            <span>{from}-{to}</span>
+        </li>
+        {/each}
+    </ul>
+</section>
+
+<section>
+    <h2>Experience</h2>
+    <ul>
+        {#each experience as { name, descriptions, from, to }}
+        <li>
+            <div class="experience-header">
+                <p>{name}</p>
+                <span>{from}-{to}</span>
+            </div>
+            <ul>
+                {#each descriptions as d}
+                <li>{d}</li>
+                {/each}
+            </ul>
+        </li>
+        {/each}       
+    </ul>
+</section>
